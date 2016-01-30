ebe296f4357dca67b128cc008f838f5a7509378c
Üsame Esendir <esendir@hotmail.com>
2016-01-30 12:31:44 +0200

    First release

---
+++ Repository.Mongo.Cached/CachedRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Rabbit.Cache;
+
+namespace Repository.Mongo
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class CachedRepository<T> : Repository<T>
+        where T : IEntity
+    {
+        public EntityCache<T> Cache { get; private set; }
+        private int CacheDuration { get; set; }
+
+        public CachedRepository(string connectionString, ICache cache) :
+            base(connectionString)
+        {
+            Cache = new EntityCache<T>(cache);
+        }
+
+        public CachedRepository(string connectionString, int cacheDuration, ICache cache) :
+            this(connectionString, cache)
+        {
+            CacheDuration = cacheDuration;
+        }
+
+        public override T Get(string id)
+        {
+            T result = default(T);
+            if (!Cache.TryGet(id, out result))
+            {
+                result = base.Get(id);
+                Cache.Set(result);
+            }
+            return result;
+        }
+
+        public override IEnumerable<T> Find(Expression<Func<T, bool>> filter)
+        {
+            IEnumerable<T> result = null;
+            string key = filter.ToString();
+            if (!Cache.TryGet(key, out result))
+            {
+                result = base.Find(filter);
+                Cache.Set(key, result, CacheDuration);
+            }
+            return result;
+        }
+
+        public override IEnumerable<T> Find(Expression<Func<T, bool>> filter, Expression<Func<T, object>> order,
+                                            int pageIndex, int size, bool isDescending)
+        {
+            IEnumerable<T> result = null;
+            string key = filter.ToString() + order + pageIndex + size + isDescending;
+            if (!Cache.TryGet(key, out result))
+            {
+                result = base.Find(filter, order, pageIndex, size, isDescending);
+                Cache.Set(key, result, CacheDuration);
+            }
+            return result;
+        }
+
+        public override void Insert(T entity)
+        {
+            base.Insert(entity);
+            Cache.Set(entity);
+        }
+
+        public override void Insert(IEnumerable<T> entities)
+        {
+            base.Insert(entities);
+            Cache.Set(entities);
+        }
+
+        public override bool Update(T entity, UpdateDefinition<T> update)
+        {
+            Cache.Remove(entity);
+            return base.Update(entity, update);
+        }
+
+        public override void Replace(T entity)
+        {
+            base.Replace(entity);
+            Cache.Set(entity);
+        }
+
+        public override void Delete(string id)
+        {
+            base.Delete(id);
+            Cache.Remove(id);
+        }
+
+    }
+}
---
+++ Repository.Mongo.Cached/EntityCache.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rabbit.Cache;
+
+namespace Repository.Mongo
+{
+    public class EntityCache<T> where T : IEntity
+    {
+        internal ICache Cache { get; private set; }
+
+        internal EntityCache(ICache cache)
+        {
+            Cache = cache;
+        }
+
+        public bool Set(T item)
+        {
+            return Set(item.Id, item);
+        }
+
+        public bool Set(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                Set(item);
+            return true;
+        }
+
+        public bool Set(string key, object value)
+        {
+            return Set(key, value, 0);
+        }
+
+        public bool Set(string key, object value, int min)
+        {
+            if (Equals(value, null))
+                return false;
+            if (Contains(key))
+                Remove(key);
+
+            return min > 0 ? Cache.Set(key, value, TimeSpan.FromMinutes(min)) : Cache.Set(key, value);
+        }
+
+        public bool Contains(T item)
+        {
+            if (Equals(item, default(T)))
+                return false;
+            return Contains(item.Id);
+        }
+
+        public bool Contains(string id)
+        {
+            bool result = false;
+            try
+            {
+                var cItem = Cache.Get<object>(id);
+                result = cItem != null;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool TryGet(string id, out T item)
+        {
+            item = default(T);
+            bool result = false;
+            try
+            {
+                var cItem = Cache.Get<object>(id);
+                if (cItem != null)
+                {
+                    item = (T)cItem;
+                    result = true;
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool TryGet(string id, out IEnumerable<T> items)
+        {
+            items = null;
+            bool result = false;
+
+            try
+            {
+                var cItem = Cache.Get<object>(id);
+                if (cItem != null)
+                {
+                    items = (IEnumerable<T>)cItem;
+                    result = true;
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool Remove(T item)
+        {
+            if (Equals(item, default(T)))
+                return false;
+            return Remove(item.Id);
+        }
+
+        public bool Remove(string id)
+        {
+            if (Contains(id))
+                return Cache.Remove(id);
+            return true;
+        }
+
+    }
+}
